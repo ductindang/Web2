97180ad06dfddb206d4142ce24a9205e1b84ffd2
Hoang Khiem <161942497+khiemHoang1410@users.noreply.github.com>
2024-05-01 17:11:16 +0700

    Add files via upload

--- site/view/home/index.php
+++ site/view/home/index.php
@@ -1,8 +1,7 @@
-<?php require "layout/header.php"?>
-
+<?php require "layout/header.php" ?>
 <div class="slideshow container-fluid">
     <div class="row">
-        <div id="myCarousel" class="carousel slide" data-ride="carsousel">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
             <ol class="carousel-indicators">
                 <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
@@ -13,11 +12,15 @@
             <!-- Wrapper for slides -->
             <div class="carousel-inner">
                 <div class="item active">
-                    <img src="../upload/slider1.webp" alt="slider 1">
+                    <img src="../upload/slider1.jpg" alt="slider 1">
                 </div>
 
                 <div class="item">
-                    <img src="../upload/slider2.webp" alt="slider 2">
+                    <img src="../upload/slider_2.jpg" alt="slider 2">
+                </div>
+
+                <div class="item">
+                    <img src="../upload/slider_3.jpg" alt="slider 3">
                 </div>
             </div>
 
@@ -31,21 +34,56 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
-
     </div>
 </div>
-
-<!-- END SLIDERSHOW -->
+<!-- END SLIDESHOW -->
 <!-- SERVICES -->
-
-
+<div class="top-services container-fluid">
+    <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6 item item-1">
+            <div class="item-inner">
+                <i  class="fa-solid fa-calendar-days icon2 "></i>
+                <a class="item-inline" title="7 NGÀY ĐỔI TRẢ" href="#">
+                    <span class="title-sv">7 NGÀY ĐỔI TRẢ</span>
+                    <span>Chăm sóc khách hàng cực tốt</span>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 item item-2">
+            <div class="item-inner">
+                <i class="fa-solid fa-truck-fast icon2"></i>
+                <a class="item-inline" title="MIỄN PHÍ SHIP" href="#">
+                    <span class="title-sv">MIỄN PHÍ SHIP</span>
+                    <span>Với dịch vụ giao hàng tiết kiệm</span>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 item item-3">
+            <div class="item-inner">
+                <i class="fa-solid fa-money-check-dollar icon2"></i>
+                <a class="item-inline" title="BÁN BUÔN NHƯ BÁN SỈ" href="#">
+                    <span class="title-sv">BÁN BUÔN NHƯ BÁN SỈ</span>
+                    <span>Giá hợp lý nhất quả đất</span>
+                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 item item-4">
+            <div class="item-inner">
+                <i class="fa-solid fa-circle-check icon2"></i>
+                <a class="item-inline" title="CHẤT LƯỢNG HÀNG ĐẦU" href="#">
+                    <span class="title-sv">CHẤT LƯỢNG HÀNG ĐẦU</span>
+                    <span>Chăm sóc bạn như người thân </span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
 <main id="maincontent" class="page-main">
     <div class="container">
         <div class="row equal">
             <div class="col-xs-12 text-center">
                 <h4 class="home-title">Sản phẩm nổi bật</h4>
             </div>
-            <!-- Gọi từ HomeController -->
             <?php foreach ($featuredProducts as $product): ?>
             <div class="col-xs-6 col-sm-3 container1">
                 <?php require "layout/product.php" ?>
@@ -62,8 +100,18 @@
             </div>
             <?php endforeach ?>
         </div>
+        <?php foreach ($categoryProducts as $categoryName => $products): ?>
+        <div class="row equal">
+            <div class="col-xs-12 text-center">
+                <h4 class="home-title"><?=$categoryName?></h4>
+            </div>
+            <?php foreach ($products as $product): ?>
+            <div class="col-xs-6 col-sm-3 container1">
+                <?php require "layout/product.php" ?>
+            </div>
+            <?php endforeach ?>
+        </div>
+        <?php endforeach ?>
     </div>
 </main>
-
-
-<?php require "layout/footer.php"?>
+<?php require "layout/footer.php" ?>
